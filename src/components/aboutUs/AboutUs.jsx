f9d687286f428730ec44e42f9a72197552e79b86
thong1403 <110025148+thong1403@users.noreply.github.com>
2023-06-29 11:05:59 +0700

    update

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./AboutUs.css";
-import imgBackgroud from "../images/Group 8 (2).png";
+import imgBackgroud from "../images/353399350_137125962717203_6462137882169683759_n.jpg";
+import imgContent from "../images/Frame 42962.png";
+import img1 from '../images/Rectangle 1.png'
+import img2 from '../images/Rectangle 2.png'
+import img3 from '../images/Rectangle 3.png'
+import img4 from '../images/Rectangle 4.png'
+
 
 export default function AboutUs() {
   return (
@@ -8,54 +14,32 @@ export default function AboutUs() {
       <img src={imgBackgroud} alt="" className="w-100" />
       <div className="container">
         <div className="row">
-          <h1 className="text-center title-about-us">ABOUT US</h1>
-          <div className="col-12">
-            <div className="row">
-              <div className="col-9 container-content">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse nec purus ex. Sed euismod enim lectus, et
-                  facilisis risus viverra id. Sed eget ipsum vitae sapien
-                  sodales accumsan vitae et purus. Pellentesque egestas dui sem,
-                  vitae cursus odio sollicitudin vel. Vivamus dui est, dignissim
-                  vitae finibus eget, iaculis quis nisi. Nulla facilisi. Mauris
-                  ultrices condimentum sapien, id pulvinar tortor vehicula eget.
-                  Vestibulum faucibus pharetra lectus in tristique. Lorem ipsum
-                  dolor sit amet, consectetur adipiscing elit. Pellentesque vel
-                  est a augue luctus mollis at quis diam. Etiam ullamcorper
-                  tempor consequat. Pellentesque iaculis tincidunt nibh, sit
-                  amet euismod eros elementum sit amet.{" "}
-                </p>
+          <h1 className="text-center title-about-us mt-5 mb-5">ABOUT US</h1>
+          <div className="col-12 col-12-content">
+            <p className="fs-2">
+              Part of SF-Technologies , we are experts in technology <br />
+              SF-Technologies exists in a group that boasts a global <br />
+              technology talent solutions business. <br />
+              We deliver smart solutions that solve business challenges and
+              create value.
+            </p>
+            <img src={imgContent} alt="" className="mt-5 mb-5 w-100" />
+            <h3 className="mt-5 mb-5">
+              We care, we share, we innovate <br />
+              Supportive environment encouraging friendliness and openness
+            </h3>
+            <div className="row container-img-content-col-3">
+              <div className="col-3">
+                <img src={img1} alt="" />
               </div>
-              <div className="col-3 img-content">
+              <div className="col-3">
+                <img src={img2} alt="" />
               </div>
-            </div>
-            <div className="row row-conten-about-us">
-            <div className="col-3 img-content">
+              <div className="col-3">
+                <img src={img3} alt="" />
               </div>
-              <div className="col-9 container-content float-right">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse nec purus ex. Sed euismod enim lectus, et
-                  facilisis risus viverra id. Sed eget ipsum vitae sapien
-                  sodales accumsan vitae et purus. Pellentesque egestas dui sem,
-                  vitae cursus odio sollicitudin vel. Vivamus dui est, dignissim
-                  vitae finibus eget, iaculis quis nisi. Nulla facilisi. Mauris
-                  ultrices condimentum sapien, id pulvinar tortor vehicula eget.
-                  Vestibulum faucibus pharetra lectus in tristique. Lorem ipsum
-                  dolor sit amet, consectetur adipiscing elit. Pellentesque vel
-                  est a augue luctus mollis at quis diam. Etiam ullamcorper
-                  tempor consequat. Pellentesque iaculis tincidunt nibh, sit
-                  amet euismod eros elementum sit amet.{" "}
-                </p>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-12 text-center build-website">
-                <h1>
-                Build your dream website today
-                </h1>
-                <p>But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her.</p>
+              <div className="col-3">
+                <img src={img4} alt="" />
               </div>
             </div>
           </div>
