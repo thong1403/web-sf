ba445d39fdede0e77ea7f1c3a41698ce7800b662
thong1403 <110025148+thong1403@users.noreply.github.com>
2023-06-29 13:54:18 +0700

    update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,6 +7,7 @@ import ContactUs from "../contactUs/ContactUs";
 import Industries from "../Industries/Industries";
 import HomePage from "./../homePage/HomePage";
 import './Home.css'
+import Services from './../services/Services';
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <div className="router-page">
         <Routes>
           <Route path="/web-sf" element={<HomePage />} />
+          <Route path="/services" element={<Services/>}/>
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/contactus" element={<ContactUs />} />
           <Route path="/Industries" element={<Industries />} />
