2802bd48a3f69dba942c4d8634824d3f86f153c3
thong1403 <110025148+thong1403@users.noreply.github.com>
2023-07-06 18:03:48 +0700

    update

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -36,57 +36,79 @@ export default function HomePage() {
               <div className="col-6 mb-5 fs-2 container-content-furuter">
                 <p>
                   =============== <br />
-                  Be one of outstanding from Vietnam, SF-Technologies is a
-                  Development & Design expert company providing superior
-                  Software, Web, Mobile and Creative Designing solutions and
-                  services globally.
+                  Technological innovation: SF-Technologies always devotes time
+                  and resources to the research and development of the latest
+                  technology. We are constantly exploring and applying
+                  disruptive technological advancements, such as artificial
+                  intelligence, machine learning, blockchain, and IoT. This
+                  helps your business pioneer the use of technology and seize
+                  new opportunities.
                 </p>
               </div>
               <div className="col-6 mb-5 fs-2 container-content-furuter">
                 <p>
                   ============== <br />
-                  Contributed by the industry’s highly professional and trained
-                  individuals, SF-Technologies has made it’s mark as a
-                  well-established IT consultor.
+                  Smart customer interactions: SF-Technologies places an
+                  emphasis on intelligent and interactive customer experiences.
+                  We provide advanced CRM solutions, intelligent chatbots and
+                  multi-channel interaction systems to help businesses build
+                  close relationships with customers. We not only meet current
+                  customer needs, but also anticipate and meet potential needs.
                 </p>
               </div>
               <div className="col-6 mb-5 fs-2 container-content-furuter">
                 <p>
                   ============== <br />
-                  Contributed by the industry’s highly professional and trained
-                  individuals, SF-Technologies has made it’s mark as a
-                  well-established IT consultor.
+                  Intelligent Data Analytics: SF-Technologies uses artificial
+                  intelligence and data analytics to help businesses capture
+                  critical information and make strategic decisions. We provide
+                  intelligent data analysis tools and systems, helping customers
+                  identify trends, predict business results, and optimize
+                  workflow.
                 </p>
               </div>
               <div className="col-6 mb-5 fs-2 container-content-furuter">
                 <p>
                   ============== <br />
-                  Contributed by the industry’s highly professional and trained
-                  individuals, SF-Technologies has made it’s mark as a
-                  well-established IT consultor.
+                  Advanced mobile technology: SF-Technologies develops mobile
+                  applications and user-friendly interfaces on mobile platforms.
+                  We help businesses connect and interact with customers
+                  conveniently and flexibly through advanced mobile
+                  applications. This enhances the customer experience and
+                  creates a seamless connection between the business and the
+                  customer.
                 </p>
               </div>
               <div className="col-6 mb-5 fs-2 container-content-furuter">
                 <p>
                   ============== <br />
-                  Contributed by the industry’s highly professional and trained
-                  individuals, SF-Technologies has made it’s mark as a
-                  well-established IT consultor.
+                  Safety and security: SF-Technologies always puts the safety
+                  and security of data first. We apply advanced security
+                  measures to ensure that the information of our customers and
+                  businesses is protected in the best way. We not only meet the
+                  highest security standards, but also help our customers comply
+                  with data privacy regulations.
                 </p>
               </div>
               <div className="col-6 mb-5 fs-2 container-content-furuter">
                 <p>
                   ============== <br />
-                  Contributed by the industry’s highly professional and trained
-                  individuals, SF-Technologies has made it’s mark as a
-                  well-established IT consultor.
+                  International expansion: SF-Technologies is not only focused
+                  on serving domestic customers, but also aims to expand
+                  internationally. We aim to be a trusted technology partner
+                  globally, working with businesses and organizations in every
+                  country, delivering innovative technology solutions and
+                  creating sustainable value.
                 </p>
               </div>
               <div className="col-6 fs-2 container-content-furuter">
                 <p>
                   ============== <br />
-                  Quality, well-managed and try to best to scope customer's
-                  needs are our Definition of Done.
+                  Continue to innovate and improve quality: SF-Technologies is
+                  committed to maintaining innovation and continuously improving
+                  the quality of technology solutions. We will pursue the latest
+                  research and development, and listen and understand the needs
+                  of our customers well to satisfy all requirements.
                 </p>
               </div>
               <div className="col-6 text-right">
@@ -111,7 +133,12 @@ export default function HomePage() {
               <div className="col-4 container-services-content mb-3">
                 <div className="text-content">
                   <p>
-                  Software development services is a complicated process to design an application or software to meet a particular business or personal objective, goal, or strategy. We work and provide personnel to ensure quality in various stages: Planning, Analysis, Product Design, Development & Implementation, Testing, and Maintenance.
+                    Software development services is a complicated process to
+                    design an application or software to meet a particular
+                    business or personal objective, goal, or strategy. We work
+                    and provide personnel to ensure quality in various stages:
+                    Planning, Analysis, Product Design, Development &
+                    Implementation, Testing, and Maintenance.
                   </p>
                 </div>
                 <div className="d-flex justify-content-around align-items-center">
@@ -122,7 +149,11 @@ export default function HomePage() {
               <div className="col-4 container-services-content mb-3">
                 <div className="text-content">
                   <p>
-                  Web development services help create all types of web-based software and ensure a great experience for web users. At SF, we professionally design, redesign and continuously support customer-facing and enterprise web apps to achieve high conversion and adoption rates.
+                    Web development services help create all types of web-based
+                    software and ensure a great experience for web users. At SF,
+                    we professionally design, redesign and continuously support
+                    customer-facing and enterprise web apps to achieve high
+                    conversion and adoption rates.
                   </p>
                 </div>
                 <div className="d-flex justify-content-around align-items-center">
@@ -133,7 +164,12 @@ export default function HomePage() {
               <div className="col-4 container-services-content mb-3">
                 <div className="text-content">
                   <p>
-                  Do you buy a commercial CMS, or do you use an open-source CMS? Or should you customize and build your own CMS based on an open-source CMS? The team at SF will help in the open-source customization of different products to suit your needs by creating/designing templates, adding custom modules, or changing the product's core functionality.
+                    Do you buy a commercial CMS, or do you use an open-source
+                    CMS? Or should you customize and build your own CMS based on
+                    an open-source CMS? The team at SF will help in the
+                    open-source customization of different products to suit your
+                    needs by creating/designing templates, adding custom
+                    modules, or changing the product's core functionality.
                   </p>
                 </div>
                 <div className="d-flex justify-content-around align-items-center">
@@ -144,7 +180,16 @@ export default function HomePage() {
               <div className="col-4 container-services-content mb-3">
                 <div className="text-content">
                   <p>
-                  SF team has extensive experience creating high-performing, digitally transformative, and feature-packed native mobile applications for Android and iOS. Whether your mobile app needs to store data in the Cloud, communicate with connected home devices, allow in-app purchases, support Apple Pay services, handle user requests with voice technology, or integrate machine learning models, we will cover your product’s entire lifecycle, from ideation, business strategy, and design to App Store Submission and build you best-in-class functionality.
+                    SF team has extensive experience creating high-performing,
+                    digitally transformative, and feature-packed native mobile
+                    applications for Android and iOS. Whether your mobile app
+                    needs to store data in the Cloud, communicate with connected
+                    home devices, allow in-app purchases, support Apple Pay
+                    services, handle user requests with voice technology, or
+                    integrate machine learning models, we will cover your
+                    product’s entire lifecycle, from ideation, business
+                    strategy, and design to App Store Submission and build you
+                    best-in-class functionality.
                   </p>
                 </div>
                 <div className="d-flex justify-content-around align-items-center">
@@ -155,7 +200,15 @@ export default function HomePage() {
               <div className="col-4 container-services-content mb-3">
                 <div className="text-content">
                   <p>
-                  The best solutions that should be designed for your organization answer your specific needs, no more and no less. SF team will innovative business solutions allow any enterprise to digitize and automate their existing tools and processes without impact on other processes in the meantime. Our expert engineers specially trained in cloud application development provides customized Enterprise cloud computing and will guarantee that all your information, data, and business processes are secured, protected, and well-guarded.
+                    The best solutions that should be designed for your
+                    organization answer your specific needs, no more and no
+                    less. SF team will innovative business solutions allow any
+                    enterprise to digitize and automate their existing tools and
+                    processes without impact on other processes in the meantime.
+                    Our expert engineers specially trained in cloud application
+                    development provides customized Enterprise cloud computing
+                    and will guarantee that all your information, data, and
+                    business processes are secured, protected, and well-guarded.
                   </p>
                 </div>
                 <div className="d-flex justify-content-around align-items-center">
@@ -166,7 +219,14 @@ export default function HomePage() {
               <div className="col-4 container-services-content mb-3">
                 <div className="text-content">
                   <p>
-                  Implementing a new ERP system is a long-term project, and it includes more than the IT part and the involvement of a software vendor. This process demands drastic changes inside the company, which probably will bring a lot of pressure. Our team of experienced business analysts, project coordinators, solution architects, and designers will create the best-adapted ERP package to drive your business development to your need.
+                    Implementing a new ERP system is a long-term project, and it
+                    includes more than the IT part and the involvement of a
+                    software vendor. This process demands drastic changes inside
+                    the company, which probably will bring a lot of pressure.
+                    Our team of experienced business analysts, project
+                    coordinators, solution architects, and designers will create
+                    the best-adapted ERP package to drive your business
+                    development to your need.
                   </p>
                 </div>
                 <div className="d-flex justify-content-around align-items-center">
@@ -177,7 +237,15 @@ export default function HomePage() {
               <div className="col-4 container-services-content mb-3">
                 <div className="text-content">
                   <p>
-                  Technology Integration Services aims to build a safe environment that provides quick access and reliable information management. Whether it is product information, marketing content, eCommerce, or B2B data, our team has the ability and experience to evaluate your technology landscape, design simple or complex integrations and deliver outcomes that will drive efficiency, improve accuracy, and allow your business to achieve its goals. Technology Integration has never been quicker & easier than that is.
+                    Technology Integration Services aims to build a safe
+                    environment that provides quick access and reliable
+                    information management. Whether it is product information,
+                    marketing content, eCommerce, or B2B data, our team has the
+                    ability and experience to evaluate your technology
+                    landscape, design simple or complex integrations and deliver
+                    outcomes that will drive efficiency, improve accuracy, and
+                    allow your business to achieve its goals. Technology
+                    Integration has never been quicker & easier than that is.
                   </p>
                 </div>
                 <div className="d-flex justify-content-around align-items-center">
@@ -188,7 +256,15 @@ export default function HomePage() {
               <div className="col-4 container-services-content mb-3">
                 <div className="text-content">
                   <p>
-                  Efficient, cost-effective, and comprehensive outsourcing services can help boost profits and reduce risk, especially in MIS (Management Information Systems). With a team of highly qualified professionals, we are always ready to serve your needs thoughtfully. Full-time, fixed-contract, unlimited on-site or remote troubleshooting, and fixed periods, we could undertake the management and operation of the Information Technology segment for on-site processing enterprise with best-adapted cost.
+                    Efficient, cost-effective, and comprehensive outsourcing
+                    services can help boost profits and reduce risk, especially
+                    in MIS (Management Information Systems). With a team of
+                    highly qualified professionals, we are always ready to serve
+                    your needs thoughtfully. Full-time, fixed-contract,
+                    unlimited on-site or remote troubleshooting, and fixed
+                    periods, we could undertake the management and operation of
+                    the Information Technology segment for on-site processing
+                    enterprise with best-adapted cost.
                   </p>
                 </div>
                 <div className="d-flex justify-content-around align-items-center">
@@ -199,7 +275,14 @@ export default function HomePage() {
               <div className="col-4 container-services-content mb-3">
                 <div className="text-content">
                   <p>
-                  If you aim to rely on IT consulting to keep pace with today’s fast-moving IT landscape. If you feel you don't have the right internal resources, enlisting IT consulting services may be the missing piece to this picture — whether for a one-time project or ongoing strategic advice and guidance. At SF, our mission is solely focused on helping our clients leverage their technology investments to improve business performance.
+                    If you aim to rely on IT consulting to keep pace with
+                    today’s fast-moving IT landscape. If you feel you don't have
+                    the right internal resources, enlisting IT consulting
+                    services may be the missing piece to this picture — whether
+                    for a one-time project or ongoing strategic advice and
+                    guidance. At SF, our mission is solely focused on helping
+                    our clients leverage their technology investments to improve
+                    business performance.
                   </p>
                 </div>
                 <div className="d-flex justify-content-around align-items-center">
