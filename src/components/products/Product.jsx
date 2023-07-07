26d43fcc8ed8543870627bc7245d1060c2426317
thong1403 <110025148+thong1403@users.noreply.github.com>
2023-07-07 13:42:08 +0700

    update

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -49,19 +49,16 @@ export default function Product() {
               <div className="col-6">
                 <img src={img3} alt="" className="w-100 " />
               </div>
-              <div className="col-6 container-content-product text-right" id="container-rigth">
+              <div
+                className="col-6 container-content-product text-right"
+                id="container-rigth"
+              >
                 <img src={img4} alt="" className="w-50 mb-5" />
                 <p>
-                  Dự án Home Rent Lorem Ipsum is simply dummy text of the
-                  printing and typesetting industry. Lorem Ipsum has been the
-                  industry's standard dummy text ever since the 1500s, when an
-                  unknown printer took a galley of type and scrambled it to make
-                  a type specimen book. It has survived not only five centuries,
-                  but also the leap into electronic typesetting, remaining
-                  essentially unchanged. It was popularised in the 1960s with
-                  the release of Letraset sheets containing Lorem Ipsum
-                  passages, and more recently with desktop publishing software
-                  like Aldus PageMaker including versions of Lorem Ipsum.
+                  Home Rent is a rental housing management system that helps
+                  landlords and tenants learn and approach each other quickly
+                  and conveniently. With Home Rent, you can easily and quickly
+                  find an apartment or hostel that suits your needs and desires.
                 </p>
               </div>
             </div>
